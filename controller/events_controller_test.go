95b8c1ba4ec96a93b12bdc53b3719ac9891073bb
Roberto Scudeller <beto.rvs@gmail.com>
2020-04-02 14:42:38 +0200

    fix controller test

--- controller/events_controller_test.go
+++ controller/events_controller_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/betorvs/sonarqube-to-gitlab-webhook/config"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 )
@@ -12,6 +13,7 @@ import (
 func TestPostEvents(t *testing.T) {
 	// Setup
 	e := echo.New()
+	config.SonarqubeSecret = "Absent"
 	req := httptest.NewRequest(http.MethodPost, "/", nil)
 	rec := httptest.NewRecorder()
 	c := e.NewContext(req, rec)
